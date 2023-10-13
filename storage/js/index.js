9037afa79f0719fc7157a8505a89d1f3a60905cc
0Midnite <104600774+0Midnite@users.noreply.github.com>
2023-10-13 11:24:42 -0400

    🗿

--- storage/js/index.js
+++ storage/js/index.js
@@ -1,16 +1,4 @@
-function openit(url) {
-    const blanked = window.open('about:blank', '_blank');
-    if (blanked) {
-        const iframe = document.createElement('iframe');
-        iframe.src = (window.location.origin + "/storage/games/" + url + "/index.html");
-        iframe.style = "position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;";
-        blanked.document.body.appendChild(iframe);
-    }
-}
-
-// -------------------------
-
-function aeae(url) {
+function ab(url) {
     const blanked = window.open('about:blank', '_blank');
     if (blanked) {
         const iframe = document.createElement('iframe');
@@ -37,7 +25,7 @@ window.onload = chrome;
 // -------------------------
 
 document.addEventListener("mousedown", function() {
-    const randomNum = Math.floor(Math.random() * 10) + 1;
+    const randomNum = Math.floor(Math.random() * 5) + 1;
     if (randomNum === 1) {
         window.open("https://www.highcpmrevenuegate.com/fag308325?key=a721cfba016e5a75613331ef54a856f3", "_blank");
     }
